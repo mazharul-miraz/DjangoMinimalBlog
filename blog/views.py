5fd6479317d3fb13d9930a338838b4a99faf7ed6
mazharul-miraz <mazharul.miraz@outlook.com>
2022-02-04 21:07:25 +0900

    base layout done

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+#home
 def home(request):
-    return render (request,"base.html")
+    return render (request,"home.html")
+
+def about(request):
+    return render (request,"about.html")
+
+def contact(request):
+    return render (request,"contact.html")
+
+def dashboard(request):
+    return render (request,"dashboard.html")
+
+def login(request):
+    return render (request,"login.html")
+
+def logout(request):
+    return render (request,"logout.html")
+
+def signup(request):
+    return render (request,"signup.html")
--- miniblogProject/urls.py
+++ miniblogProject/urls.py
@@ -19,5 +19,12 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home,name='home')
+    path('', views.home, name='about'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    
 ]
