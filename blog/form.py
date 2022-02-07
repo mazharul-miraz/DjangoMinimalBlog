e2cd80c8a46cb59b4b90253db84e4e8958c3e593
mazharul-miraz <mazharul.miraz@outlook.com>
2022-02-07 23:32:20 +0900

    add uuser form

--- blog/form.py
+++ blog/form.py
@@ -1,11 +1,20 @@
+from cProfile import label
 from statistics import mode
+from tkinter import Widget
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from pyrsistent import field
 
 class SignUpForm(UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Confirm Password(again)',
+    widget = forms.PasswordInput())
+    
+    
     class Meta:
         model = User
         fields = ['username', 'first_name','last_name','email']
+        labels = {'first_name':'First Name','last_name':'First Name',
+                  'email':'Email'}
         
