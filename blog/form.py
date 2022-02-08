e4520923f22a2b3fb3131ab385d5413ca7eee3f0
mazharul-miraz <mazharul.miraz@outlook.com>
2022-02-08 23:56:37 +0900

    signup bug fiexed because a holy men get into my room :)

--- blog/form.py
+++ blog/form.py
@@ -10,16 +10,14 @@ class SignUpForm(UserCreationForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
     password2 = forms.CharField(label='Confirm Password(again)',
     widget = forms.PasswordInput())
-    
-    
-class Meta:
-    model = User
-    fields = ['username', 'first_name','last_name','email']
-    labels = {'first_name': 'First Name', 'last_name': 'Last Name',
-    'email': 'Email'}
-    widgets = {'username':forms.TextInput(attrs={'class':'form-control'}),
-    'first_name':forms.TextInput(attrs={'class':'form-control'}),
-    'last_name':forms.TextInput(attrs={'class':'form-control'}),
-    'email':forms.EmailInput(attrs={'class':'form-control'}),
+    class Meta:
+        model = User 
+        fields = ['username', 'first_name','last_name','email']
+        labels = {'first_name': 'First Name', 'last_name': 'Last Name',
+        'email': 'Email'}
+        widgets = {'username':forms.TextInput(attrs={'class':'form-control'}),
+        'first_name':forms.TextInput(attrs={'class':'form-control'}),
+        'last_name':forms.TextInput(attrs={'class':'form-control'}),
+        'email':forms.EmailInput(attrs={'class':'form-control'}),
 }
         
