607272b71f3dc983d6943536eb02e9387f55d404
mazharul-miraz <mazharul.miraz@outlook.com>
2022-02-10 23:01:16 +0900

    add login

--- blog/form.py
+++ blog/form.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UsernameField,AuthenticationForm
 from django.contrib.auth.models import User
+from more_itertools import strip
 from pyrsistent import field
 
 class SignUpForm(UserCreationForm):
@@ -17,4 +18,9 @@ class SignUpForm(UserCreationForm):
         'last_name':forms.TextInput(attrs={'class':'form-control'}),
         'email':forms.EmailInput(attrs={'class':'form-control'}),
 }
-          
+        
+        
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={'class':'form-control', 'autofocus':True})),
+    password = forms.CharField(label=("password"), strip=False, widget=forms.PasswordInput(attrs={'class':'form-control', 'autofocus':True, 'autocomplete':'current-password'}))
+
