08031b636a105c1131e05242ac5bf070cb8cf8bc
mazharul-miraz <mazharul.miraz@outlook.com>
2022-02-10 22:16:54 +0900

    add signuup form

--- blog/form.py
+++ blog/form.py
@@ -1,15 +1,12 @@
-from cProfile import label
-from statistics import mode
-from tkinter import Widget
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField,AuthenticationForm
 from django.contrib.auth.models import User
 from pyrsistent import field
 
 class SignUpForm(UserCreationForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Confirm Password(again)',
-    widget = forms.PasswordInput())
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Confirm Password(again)', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    
     class Meta:
         model = User 
         fields = ['username', 'first_name','last_name','email']
@@ -20,4 +17,4 @@ class SignUpForm(UserCreationForm):
         'last_name':forms.TextInput(attrs={'class':'form-control'}),
         'email':forms.EmailInput(attrs={'class':'form-control'}),
 }
-        
+          
--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
-    desc = models.TextField()
+    desc = models.TextField() 
